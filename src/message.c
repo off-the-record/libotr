edf738fa18e2c3afea7213a474b109b7a53a52b4
Ian Goldberg <iang@cs.uwaterloo.ca>
2012-08-24 14:41:08 -0400

    Trigger a context dump when a special debug string is typed

--- src/message.c
+++ src/message.c
@@ -35,6 +35,16 @@
 #include "sm.h"
 #include "instag.h"
 
+#if OTRL_DEBUGGING
+#include <stdio.h>
+
+/* If OTRL_DEBUGGING is on, and the user types this string, the current
+ * context and its siblings will be dumped to stderr. */
+#define OTRL_DEBUGGING_DEBUGSTR "?OTR!"
+
+void otrl_context_siblings_dump(FILE *f, const ConnContext *context);
+#endif
+
 /* The API version */
 extern unsigned int otrl_api_version;
 
@@ -239,6 +249,20 @@ gcry_error_t otrl_message_sending(OtrlUserState us,
 	err =  gcry_error(GPG_ERR_NO_ERROR);
 	goto fragment;
     }
+
+#if OTRL_DEBUGGING
+    /* If the user typed the magic debug string, dump this context and
+     * its siblings. */
+    if (strstr(original_msg, OTRL_DEBUGGING_DEBUGSTR)) {
+	otrl_context_siblings_dump(stderr, context);
+
+	*messagep = strdup("");
+	if (!(*messagep)) {
+	    err = gcry_error(GPG_ERR_ENOMEM);
+	    goto fragment;
+	}
+    }
+#endif
 
     /* If this is an OTR Query message, don't encrypt it. */
     if (otrl_proto_message_type(original_msg) == OTRL_MSGTYPE_QUERY) {
